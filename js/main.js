556739fcf0e5fa763160bfeb850beae6b7ec7a7c
RomanRenaud <romanrenaud13@gmail.com>
2026-07-04 00:48:46 -0300

    feat: se agrega un contador al final de programas, se amplía y agrega el botón de modo oscuro a las demás páginas

--- js/main.js
+++ js/main.js
@@ -5,25 +5,31 @@ botonModo.addEventListener("click", () => {
     document.body.classList.toggle("dark-mode");
 
     if (document.body.classList.contains("dark-mode")) {
-        botonModo.textContent = "Modo claro";
+        botonModo.textContent = "🌞 Modo claro";
     } else {
-        botonModo.textContent = "Modo oscuro";
+        botonModo.textContent = "🌙 Modo oscuro";
     }
 });
+
+// =================
+// CONTADOR
+// =================
+let numero = 0;
+const contador = setInterval(() => {
+    numero++;
+    const span = document.getElementById("contador-rescates");
+    span.textContent = numero;
+}, Math.random() * 5000);
+
 // =================
 // OCULTAR ELEMENTO
 // =================
 
 const botonLeer = document.getElementById("btn-leer");
-const textoOculto = document.getElementById("texto-extra");
-
-botonLeer.addEventListener("click", () => {
-    
-        textoOculto.classList.toggle("d-none");
-
-    if (!textoOculto.classList.contains("d-none")) {
-        botonLeer.textContent = "Leer menos";
-    } else {
-        botonLeer.textContent = "Leer más";
-    }
-});
+if (botonLeer) {
+  const textoOculto = document.getElementById("texto-extra");
+  botonLeer.addEventListener("click", () => {
+    textoOculto.classList.toggle("d-none");
+    botonLeer.textContent = textoOculto.classList.contains("d-none") ? "Leer más" : "Leer menos";
+  });
+}
